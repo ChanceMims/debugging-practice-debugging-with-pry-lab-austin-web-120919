e2ebee1c668ee687abe387bace649d3f0884c2e0
Chance Mims <chancemims1@gmail.com>
2019-12-09 02:02:50 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,7 +9,7 @@ def turtles
 end
 
 def turtle_traits(turtles)
-  turtles.map { |turtle| Array(turtle[:traits]) if turtle[:traits].kind_of?(Array) == false }
+  new_turtle = turtles.map { |turtle| Array(turtle[:traits]) if turtle[:traits].kind_of?(Array) == false }
   turtles.map do |turtle|
     turtle[:traits].each do |trait|
       trait
